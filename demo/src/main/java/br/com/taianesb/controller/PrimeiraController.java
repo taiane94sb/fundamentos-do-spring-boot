8aef5ba5ea902c1f3cea37250e3135be3fdebf6d
taiane94sb <taiane19sb94@gmail.com>
2023-10-21 08:48:20 -0300

    path params

--- demo/src/main/java/br/com/taianesb/controller/PrimeiraController.java
+++ demo/src/main/java/br/com/taianesb/controller/PrimeiraController.java
@@ -1,8 +1,13 @@
 package br.com.taianesb.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/primeiraController")
@@ -11,6 +16,21 @@ public class PrimeiraController {
     @GetMapping("/primeiroMetodo")
     public String primeiroMetodo() {
         return "Meu primeiro método api rest...";
+    }
+
+    @GetMapping("/primeiroMetodoPathVariable/{id}")
+    public String primeiroMetodoPathVariable(@PathVariable String id) {
+        return "O parâmetro é: " + id;
+    }
+
+    @GetMapping("/primeiroMetodoRequestParam")
+    public String primeiroMetodoRequestParam(@RequestParam String id) {
+        return "O parâmetro é: " + id;
+    }
+
+    @GetMapping("/primeiroMetodoRequestParam2")
+    public String primeiroMetodoRequestParam2(@RequestParam Map<String, String> allParams) {
+        return "Os parâmetro são: " + allParams.entrySet();
     }
 
 }
