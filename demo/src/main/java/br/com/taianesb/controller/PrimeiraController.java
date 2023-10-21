35ef3766a9eed7060fc74e0cd0f4a0b79b253795
taiane94sb <taiane19sb94@gmail.com>
2023-10-21 08:26:41 -0300

    component scan

--- demo/src/main/java/br/com/taianesb/controller/PrimeiraController.java
+++ demo/src/main/java/br/com/taianesb/controller/PrimeiraController.java
@@ -1,4 +1,4 @@
-package br.com.taianesb.demo;
+package br.com.taianesb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- demo/src/main/java/br/com/taianesb/demo/DemoApplication.java
+++ demo/src/main/java/br/com/taianesb/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package br.com.taianesb.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.taianesb")
 public class DemoApplication {
 
 	public static void main(String[] args) {
