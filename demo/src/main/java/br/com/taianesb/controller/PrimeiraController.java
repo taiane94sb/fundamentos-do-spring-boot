08d76ac9378821b628d2802909eb26374c617c33
taiane94sb <taiane19sb94@gmail.com>
2023-10-22 10:46:04 -0300

    body params

--- demo/src/main/java/br/com/taianesb/controller/PrimeiraController.java
+++ demo/src/main/java/br/com/taianesb/controller/PrimeiraController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +34,12 @@ public class PrimeiraController {
     public String primeiroMetodoRequestParam2(@RequestParam Map<String, String> allParams) {
         return "Os parâmetro são: " + allParams.entrySet();
     }
+
+    @PostMapping("primeiroMetodoBodyParam")
+    public String primeiroMetodoBodyParam(@RequestBody Usuario usuario) {
+        return "O parâmetro é: " + usuario.username;
+    }
+
+    record Usuario(String username) {}
 
 }
